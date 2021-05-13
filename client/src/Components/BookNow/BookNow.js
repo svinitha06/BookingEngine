c37e41b625db8eb1f552c78409aab6c8ce5614b1
sh20184278 <sherin.17@wipro.com>
2021-05-13 11:30:22 +0530

    BookNow

--- client/src/Components/BookNow/BookNow.js
+++ client/src/Components/BookNow/BookNow.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 import { Card } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react'
+import { Button,Icon } from 'semantic-ui-react'
 import RoomNum from './RoomNum'
 import ReactDOM from "react-dom";
 import './BookNow.css'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class BookNow extends React.Component {
     constructor(props) {
@@ -28,34 +29,45 @@ class BookNow extends React.Component {
                   <div className="row">
                     <div className="col">
                       <div className="row">
-                        <div className="col">
+                        <div className="col Text">
                           Arrival
-                          <input type="date" id="arrDate"/>
+                          <input type="date" id="arrDate" />
                         </div>
-                        <div className="col">
+                        <div className="col Text">
                           Departure
-                          <input type="date" id="depDate"/>
+                          <input type="date" id="depDate" />
                         </div>
                       </div>
                     </div>
                     <div className="col">
-                      
                       <div className="room-drop">
-                        <label for="rooms">Rooms</label>
-                        <select name="Rooms" placeholder="Select Value" id="rooms" onChange={this.setRooms}>
-                          <option value="0" >Select value</option>
+                        <label className="Text" for="rooms">
+                          Rooms
+                        </label>
+                        <select
+                          name="Rooms"
+                          placeholder="Select Value"
+                          id="rooms"
+                          onChange={this.setRooms}
+                        >
+                          <option value="0">No.of Room(s)</option>
                           <option value="1">1</option>
                           <option value="2">2</option>
                           <option value="3">3</option>
                           <option value="4">4</option>
                           <option value="5">5</option>
                         </select>
-                        <div id="all-rooms">
+                        <div className="row ml-3">
+                          <div id="all-rooms"></div>
                         </div>
                       </div>
                     </div>
-                    <br/>
-                    <Button fluid color={"blue"} className="search">Search</Button>
+                    <Button fluid animated inverted color="olive">
+                      <Button.Content visible>Search</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name="arrow right" />
+                      </Button.Content>
+                    </Button>
                   </div>
                 </Card.Content>
               </Card>
--- client/src/Components/BookNow/RoomNum.js
+++ client/src/Components/BookNow/RoomNum.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import './BookNow.css'
 
 const RoomNum = (props) => {
     var list = [];
@@ -11,8 +12,8 @@ const RoomNum = (props) => {
         
     {list.map(function(i){
         return (<div >
-            <h6>Room-{i}</h6>
-            <div class="col-md-offset-5 col-md-10 d-flex">
+            <div id="RoomNum">Room-{i}</div>
+            <div class="col-md-offset-5 col-md-10 d-flex ">
                 <label  >Adults</label>
                 <input type="number" min="1" max="7" className="col-md-5 mx-2" name="adults" />
                 <label>Child</label>
