5748466c5d9f49412262a2ef902f577b70e5ed2f
ayushi20184 <ayushi.verma6@wipro.com>
2021-05-18 11:18:31 +0530

    Room display with button and changed date range picker

--- client/src/Components/DisplayTile/DisplayTile.js
+++ client/src/Components/DisplayTile/DisplayTile.js
@@ -1,25 +1,86 @@
 import React, { Component } from "react";
 import ImageTile from "../DisplayTile/Image.jpeg";
+import Details from "./roomTypeDetail.json";
 import "./DisplayTile.css";
 
 class DisplayTile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 1,
+      // price1: 0,
+    };
+  }
+
+  handleMinus = () => {
+    if (this.state.count > 1) {
+      this.setState({
+        count: this.state.count - 1,
+        // price1: this.state.price1 * this.state.count,
+      });
+    }
+  };
+
+  handlePlus = () => {
+    if (this.state.count < 6) {
+      this.setState({
+        count: this.state.count + 1,
+        // price1: this.state.price1 * this.state.count,
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <div className="wrapper">
-          <div>
-            <img className="ImageTile" src={ImageTile}></img>
-          </div>
-          <div>
-            <h2>Super AC</h2>
-            <p>
-              Renovated & are located at the heart of the resort close to the
-              pool and activity area with a separate veranda for a relaxing
-              sit-out. They are well furnished with one double bed and fully
-              equipped with air-conditioner, LCD TV, tea/coffee maker,
-              Refrigerator and telephone. Occupancy of a minimum 5 to 6 Persons.
-            </p>
-          </div>
+      <div className="display">
+        <div>
+          {Details.map((post, index) => {
+            return (
+              <div className="wrapper">
+                <div className="ImageTile">
+                  <img src={ImageTile}></img>
+                </div>
+                <div className="childWrapper" key={post.id}>
+                  <div>
+                    <h2>{post.Rname}</h2>
+                    <p>{post.Desc}</p>
+                  </div>
+                  <div className="price">
+                    <div className="block1">
+                      <p>Starting from</p>
+                      <h2>{post.price}</h2>
+                    </div>
+                    <div className="button-price">
+                      <button className="thebutton" onClick={this.handleMinus}>
+                        -
+                      </button>
+                      <button className="thebutton">{this.state.count}</button>
+                      <button className="thebutton" onClick={this.handlePlus}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div></div>
+                <div className="reserve-content">
+                  <span>
+                    <h3>Total Rooms :</h3>
+                  </span>
+                  <span>
+                    <h3>{this.state.count}</h3>
+                  </span>
+                  <span>
+                    <h3>Total Price :</h3>
+                  </span>
+                  <span>
+                    <h2 className="priceh2">{post.price * this.state.count}</h2>
+                  </span>
+                  <button className="reserve">Reserve</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
