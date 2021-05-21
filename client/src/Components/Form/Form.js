b11a1c07daea4d71d9dd203b3d987a37053ef9ae
ayushi20184 <ayushi.verma6@wipro.com>
2021-05-21 10:53:48 +0530

    Form added

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./Form.css";
-
+import { get } from "lodash";
+import { connect } from "react-redux";
+import { DateRangePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { NavLink } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 export class Form extends Component {
   constructor(props) {
     super(props);
@@ -10,9 +14,16 @@ export class Form extends Component {
       lastName: "",
       email: "",
       contact: "",
+      gender: "",
+      address: "",
     };
   }
-
+  componentDidMount() {
+    this.setState({
+      start: this.props.dateRange.start,
+      end: this.props.dateRange.end,
+    });
+  }
   handleFirstName = (event) => {
     this.setState({
       firstName: event.target.value,
@@ -35,14 +46,50 @@ export class Form extends Component {
       contact: event.target.value,
     });
   };
+  handleGender = (event) => {
+    this.setState({
+      gender: event.target.value,
+    });
+  };
+  handleAddress = (event) => {
+    this.setState({
+      address: event.target.value,
+    });
+  };
 
   render() {
+    const minValue = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate()
+    );
     return (
-      <div>
+      <div className="container-form">
         <div className="form">
+          <div className="form-date">
+            <Button className="form-btn" as={NavLink} to="/basiclayout">
+              Back
+            </Button>
+            <div className="final-date">
+              <span>
+                <h6>Check-in / Check-out</h6>
+              </span>
+              <DateRangePickerComponent
+                placeholder="Check-in/Check-out"
+                startDate={this.state.start}
+                endDate={this.state.end}
+                min={minValue}
+                format={"dd-MMM-yy"}
+                color={"black"}
+                className="form-datepicker"
+                id="datepickeridform"
+              ></DateRangePickerComponent>
+            </div>
+          </div>
+
           <form>
             <h2>Book your Room</h2>
-            <div>
+            <div className="form-contents">
               <label>First Name</label>
               <input
                 type="text"
@@ -58,7 +105,7 @@ export class Form extends Component {
                 type="text"
                 placeholder="Last Name"
                 value={this.state.lastName}
-                onChange={this.handlelastName}
+                onChange={this.handleLastName}
               ></input>
               <br />
               <label>Email Address</label>
@@ -78,6 +125,32 @@ export class Form extends Component {
               ></input>
               <br />
               <label>Gender</label>
+              <label>
+                Male
+                <input
+                  type="radio"
+                  value="Male"
+                  checked={this.state.gender === "Male"}
+                  onChange={this.handleGender}
+                ></input>
+              </label>
+              <label>
+                Female
+                <input
+                  type="radio"
+                  value="Female"
+                  checked={this.state.gender === "Female"}
+                  onChange={this.handleGender}
+                ></input>
+              </label>
+              <br></br>
+              <h4>Billing address</h4>
+              <label>Address</label>
+              <input
+                type="text-area"
+                value={this.state.address}
+                onChange={this.handleAddress}
+              ></input>
             </div>
           </form>
         </div>
@@ -85,5 +158,9 @@ export class Form extends Component {
     );
   }
 }
+const mapStateToProps = (state) => ({
+  dateRange: get(state, "dateRange", []),
+  roomRange: get(state, "roomRange", []),
+});
 
-export default Form;
+export default connect(mapStateToProps, null)(Form);
