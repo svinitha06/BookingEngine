e55a2a041456aab450c00dd9fc071ba0d216c097
SayantikaPaul12 <sayantika.paul1@wipro.com>
2021-05-26 11:03:58 +0530

    saved changes1

--- server/routes/AddingRooms.js
+++ server/routes/AddingRooms.js
@@ -1,77 +1,47 @@
-const express = require("express");
+const express = require('express');
 
-const RoomTypeMaster = require("../Models/RoomTypeMaster");
-const mongoose = require("mongoose");
+const RoomTypeMaster = require('../Models/RoomTypeMaster');
+const mongoose = require('mongoose');
 
-mongoose.connect(
-  `mongodb+srv://sathishm2408:${encodeURIComponent(
-    "S@chu2408"
-  )}@cluster0.ifzlg.mongodb.net/BookingEngine?retryWrites=true&w=majority`,
-  {
+
+mongoose.connect(`mongodb+srv://sathishm2408:${encodeURIComponent('S@chu2408')}@cluster0.ifzlg.mongodb.net/BookingEngine?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+    useUnifiedTopology: true
+})
 
-const router = express.Router();
+
+const router = express.Router()
 
 //get back all the rooms
-router.get("/getRoomType", async (req, res) => {
-  try {
-    const posts = await RoomTypeMaster.find();
-    res.json(posts);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+router.get('/getRoomType',async(req,res) =>{
+    try{
+        const posts = await RoomTypeMaster.find();
+        res.json(posts);
+    } catch (err){
+        res.json({message: err});
+    }
+})
+
 
 //Submit a post for rooms
-router.post("/addRoomType", async (req, res) => {
-  const post = new RoomTypeMaster({
-    PropertyId: req.body.PropertyId,
-    roomType: req.body.roomType,
-    roomImage: req.body.roomImage,
-    description: req.body.description,
-    numberofRooms: req.body.numberofRooms,
-    availability: req.body.availability,
-  });
-  try {
-    const savedPost = await post.save();
-    res.json(savedPost);
-  } catch (err) {
-    res.status(400).send({ message: err });
-  }
-});
+router.post('/addRoomType', async (req, res) => {
+    const post = new RoomTypeMaster({
+        PropertyId: req.body.PropertyId,
+        roomType: req.body.roomType,
+        roomImage: req.body.roomImage,
+        description: req.body.description,
+        numberofRooms: req.body.numberofRooms,
+        availability: req.body.availability
 
-<<<<<<< HEAD
-//Getting rooms by ProprtyID
-router.get("/:PropertyId", async (req, res) => {
-  try {
-    const post = await RoomTypeMaster.find({
-      PropertyId: req.params.PropertyId,
     });
-    res.json(post);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err){
+        res.status(400).send({message:err});
+    }
 });
-=======
 
-<<<<<<< Updated upstream
-//Getting rooms by Proprty ID
-//  router.get('/getRoomType/:availability', async (req, res) => {
-//      try{
-//          const post = await RoomTypeMaster.find({availability: {$gte : 11}});
-//          res.json(post);
-//      } catch(err){
-//          res.status(400).send(err)        
-//      }
-//  });
-
->>>>>>> 658cff62b2d6750ccfbd1dc494abfaacd63792ed
-
-module.exports = router;
-=======
 
 
 //getting rooms by PropertyId
@@ -86,135 +56,5 @@ module.exports = router;
  });
 
 
- //import React, { Component } from 'react';
-//  class Availablility extends Component {
-//     constructor() {
-//       super();
-//       this.state = {
-//         availablility: '',
-//         isLoading: true,
-//       };
-//     }
-  
-    // componentDidMount() {
-    //   axios
-    //     .get('http://localhost:5000/rooms/getRoomType/1')
-    //     .then(({ data }) => {
-    //       console.log(data);
-    //       this.setState({
-    //         availablility: data.availablility,
-    //         isLoading: false
-    //       });
-    //     })
-    //     .catch(err => {});
-    // };
-//      componentDidMount() {
-//          axios
-//              .get('http://localhost:5000/rooms/getRoomType/1')
-//              .then(({ data }) => {
-//              var sum = 0;
-//              if(typeof data == 'object'){
-//                  data.forEach(rooms => {
-//                      sum += parseFloat(rooms.availability);
-//                 });
-//             }
-//             this.setState({
-//                 availability: sum,
-//                 isLoading: false
-//             });
-//         })
-//         .catch(err => {});
-//     }
-//  }
-//exports default RoomTypeMaster;
-
-
-// const totalz2 = () => {
-//     let total2 = 0;
-//     for (let i = 0; i < props.availablility.length; i++) {
-//       total2 += props.availablility[i].RoomTypeMaster;
-//     }
-//     //return total2;
-//     console.log(total2);
-//   };
-
-
-// router.get('/roomsavailability/:PropertyId', async (req, res) => {
-//     try{
-//         const post = await RoomTypeMaster.find({PropertyId:(req.params.PropertyId)});
-//         post.forEach(({ availability }) => {
-//             var sum = 0;
-            
-//                 post.forEach(post => {
-//                     sum += parseFloat(post.availability);
-//                });
-//             })
-            
-//         res.json(sum);
-//         } catch(err){
-//         res.status(400).send(err)
-        
-//     }
-// });
-
-// router.get('/roomsavailability/:PropertyId', async (req, res) => {
-//     try{
-//         const post = await RoomTypeMaster.find({PropertyId:(req.params.PropertyId)});
-        
-//         var sum = 0;
-            
-//         post.forEach(rooms => {
-//                     sum += parseFloat(rooms.availability);
-//                });
-            
-            
-//         res.json(sum);
-//     } catch(err){
-//         res.status(400).send(err)
-        
-//     }
-// });
-
-//   /api/v1/products?price=gte:5.00
-
-
-// router.get('/getRoomType/:PropertyId', async (req, res) => {
-//      try{
-//          const post = await RoomTypeMaster.find({PropertyId:(req.params.PropertyId)});
-//          res.json(post);
-//      } catch(err){
-//          res.status(400).send(err)
-         
-//      }
-//  });
-
-// router.get('/getRoomType/:requiredRooms', async (req, res) => {
-//     try{
-//         const post = await RoomTypeMaster.find({availability:(req.params.availability)});
-        
-        //var sum = 0;
-            
-       // post.forEach(rooms => {
-        //            sum += parseFloat(rooms.availability);
-        //       });
-            
-            
-//         res.json(post);
-//     } catch(err){
-//         res.status(400).send(err)
-        
-//     }
-// });
-
-// router.get('/getRoomType/:availability', async (req, res) => {
-//     try{
-//         const post = await RoomTypeMaster.find({availability:(req.params.availability)});
-//         res.json(post);
-//     } catch(err){
-//         res.status(400).send(err)
-        
-//     }
-// });
 
 module.exports = router;
->>>>>>> Stashed changes
--- server/routes/property.js
+++ server/routes/property.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const propertyMaster = require("../Models/propertyMaster");
 const counters = require("../Models/counters");
 const mongoose = require("mongoose");
@@ -11,19 +10,6 @@ mongoose.connect(
     "S@chu2408"
   )}@cluster0.ifzlg.mongodb.net/BookingEngine?retryWrites=true&w=majority`,
   {
-=======
-const propertyMaster = require('../Models/propertyMaster');
-const counters = require('../Models/counters');
-const mongoose = require('mongoose');
-const express = require('express');
-<<<<<<< Updated upstream
- 
-//const { response} = require ('express');
- 
-=======
->>>>>>> Stashed changes
-mongoose.connect(`mongodb+srv://sathishm2408:${encodeURIComponent('S@chu2408')}@cluster0.ifzlg.mongodb.net/BookingEngine?retryWrites=true&w=majority`, {
->>>>>>> 658cff62b2d6750ccfbd1dc494abfaacd63792ed
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -36,72 +22,8 @@ const router = express.Router();
 // router.post('/delete', isAuth, cartController.removeFromCart);
 // router.post('/update', isAuth, cartController.updateCart);
 // router.post('/sync', isAuth, cartController.syncCart);
-<<<<<<< HEAD
 
 // GET API
-=======
- 
-// GET API  
- 
-router.get('/Property',function(req,res)
-{
-    try{
-        propertyMaster.find(function(err,response){
-            if(err)
-            res.status(400).send(err)
-            else
-            res.send(response)
-        })
-    }
-    catch(error){
-        res.send(error)
-    }
-})
- 
-router.post('/addProperty', async(req, res) => {
-    console.log("get req", req.body)
-    try{
-        let oldCount =  await counters.findOne({
-            field: "PropertyId"
-        });
-        console.log("oldCount",oldCount);
-        let newCount = await counters.findOneAndUpdate({
-            field: "PropertyId"
-        },
-        {
-            count: oldCount.count + 1
-        },{
-            new: true
-        })
-        console.log("newCount",newCount);
- 
-    var newProperty = new propertyMaster({
-        PropertyId: newCount.count,
-        name: req.body.name,
-        Image: req.body.Image,
-        location: req.body.location,
-        description: req.body.description,
-        ratings: req.body.ratings,
-        website: req.body.website,
-        contact: req.body.contact,
-        Address: req.body.Address
-    })
-    newProperty.save(function (err, Person) {
-        if (err)
-            res.status('400').send(err)
-        else
-            res.send(Person)
-    })
-}
-catch(error){
-    console.log("catch",error)
-}
-});
-<<<<<<< Updated upstream
->>>>>>> 658cff62b2d6750ccfbd1dc494abfaacd63792ed
-=======
- 
->>>>>>> Stashed changes
 
 router.get("/Property", function (req, res) {
   try {
@@ -114,7 +36,6 @@ router.get("/Property", function (req, res) {
   }
 });
 
-<<<<<<< Updated upstream
 router.post("/addProperty", async (req, res) => {
   console.log("get req", req.body);
   try {
@@ -135,7 +56,6 @@ router.post("/addProperty", async (req, res) => {
     );
     console.log("newCount", newCount);
 
-<<<<<<< HEAD
     var newProperty = new propertyMaster({
       PropertyId: newCount.count,
       name: req.body.name,
@@ -156,9 +76,6 @@ router.post("/addProperty", async (req, res) => {
   }
 });
 
-=======
- 
->>>>>>> 658cff62b2d6750ccfbd1dc494abfaacd63792ed
 //Getting property based on locations
 router.get("/Property/:location", async (req, res) => {
   try {
@@ -168,39 +85,5 @@ router.get("/Property/:location", async (req, res) => {
     res.status(400).send(err);
   }
 });
-<<<<<<< HEAD
-=======
-//Getting property based on locations & availability
-router.get('/Property/search', async (req, res) => {
-    console.log(req.headers)
-    try{
-        let props = propertyMaster.find({location:(req.headers.location)});
-        let newProps =props.filter( (prop) =>{
-
-            let totalAvailability = 0;
-            
-            let post =  RoomTypeMaster.find({PropertyId:(prop.PropertyId)})
-            
-            post.forEach( room => {
-                totalAvailability +=(room.availability)
-            })
-            
-            if(totalAvailability >= (req.headers.roomsRequired) )
-            
-            return true
-            
-            });
-            res.send(newProps)
-        
-    } catch(err){
-        res.status(400).send(err)
-        
-    }
-});
-
->>>>>>> Stashed changes
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 658cff62b2d6750ccfbd1dc494abfaacd63792ed
