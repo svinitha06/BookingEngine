f7ce9bb00943c9080ae62f62c8be7e2668f64ac2
SayantikaPaul12 <sayantika.paul1@wipro.com>
2021-05-27 11:18:45 +0530

    fetching props by location and availability

--- server/routes/property.js
+++ server/routes/property.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const propertyMaster = require('../Models/propertyMaster');
 const counters = require('../Models/counters');
 const mongoose = require('mongoose');
@@ -12,11 +13,37 @@ mongoose.connect(`mongodb+srv://sathishm2408:${encodeURIComponent('S@chu2408')}@
 })
  
 const router = express.Router()
+=======
+const propertyMaster = require("../Models/propertyMaster");
+const counters = require("../Models/counters");
+
+const RoomTypeMaster = require('../Models/RoomTypeMaster');
+const mongoose = require("mongoose");
+const express = require("express");
+
+
+
+//const { response} = require ('express');
+
+mongoose.connect(
+    `mongodb+srv://sathishm2408:${encodeURIComponent(
+        "S@chu2408"
+    )}@cluster0.ifzlg.mongodb.net/BookingEngine?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    }
+);
+
+const router = express.Router();
+>>>>>>> Stashed changes
 // router.get('/', isAuth, cartController.getCart);
 // router.post('/add', isAuth, cartController.addToCart);
 // router.post('/delete', isAuth, cartController.removeFromCart);
 // router.post('/update', isAuth, cartController.updateCart);
 // router.post('/sync', isAuth, cartController.syncCart);
+<<<<<<< Updated upstream
  
 // GET API  
  
@@ -86,6 +113,90 @@ router.get('/Property/:location', async (req, res) => {
     } catch(err){
         res.status(400).send(err)
         
+=======
+
+// GET API
+
+router.get("/Property", function (req, res) {
+    try {
+        propertyMaster.find(function (err, response) {
+            if (err) res.status(400).send(err);
+            else res.send(response);
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+router.post("/addProperty", async (req, res) => {
+    console.log("get req", req.body);
+    try {
+        let oldCount = await counters.findOne({
+            field: "PropertyId",
+        });
+        console.log("oldCount", oldCount);
+        let newCount = await counters.findOneAndUpdate(
+            {
+                field: "PropertyId",
+            },
+            {
+                count: oldCount.count + 1,
+            },
+            {
+                new: true,
+            }
+        );
+        console.log("newCount", newCount);
+
+        var newProperty = new propertyMaster({
+            PropertyId: newCount.count,
+            name: req.body.name,
+            Image: req.body.Image,
+            location: req.body.location,
+            description: req.body.description,
+            ratings: req.body.ratings,
+            website: req.body.website,
+            contact: req.body.contact,
+            Address: req.body.Address,
+        });
+        newProperty.save(function (err, Person) {
+            if (err) res.status("400").send(err);
+            else res.send(Person);
+        });
+    } catch (error) {
+        console.log("catch", error);
+    }
+});
+
+//Getting property based on locations and availability
+router.get('/Property/search', async (req, res) => {
+    try {
+        let props = await propertyMaster.find({ location: (req.headers.location) });
+        let newProps = []
+        await props.forEach(async (prop) => {
+            console.log("3")
+            let totalAvailability = 0;
+
+            post = await RoomTypeMaster.find({ PropertyId: (prop.PropertyId) })
+            post.forEach(room => {
+                console.log("4")
+                totalAvailability += (room.availability)
+
+            })
+
+            if (Number(totalAvailability) >= Number(req.headers.roomsrequired))
+                newProps.push(prop)
+        })
+        mycallback = () => {
+            console.log("2", newProps)
+            res.send(newProps)
+        }
+        setTimeout(mycallback, 4000)
+
+    } catch (err) {
+        res.status(400).send(err)
+
+>>>>>>> Stashed changes
     }
 });
    
