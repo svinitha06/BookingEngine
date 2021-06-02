a780224aa5fec6e6a5653ab3c1d80ae9e487b1b4
ayushi20184 <ayushi.verma6@wipro.com>
2021-06-02 16:13:28 +0530

    hotel detail in form added

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import { DateRangePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { NavLink } from "react-router-dom";
 import { Button } from "semantic-ui-react";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import HotelDetail from "./HotelDetail";
 export class Form extends Component {
   constructor(props) {
     super(props);
@@ -16,7 +18,8 @@ export class Form extends Component {
       email: "",
       contact: "",
       gender: "",
-      address: ""
+      address: "",
+      hotelFlag: true,
     };
   }
   componentDidMount() {
@@ -24,7 +27,14 @@ export class Form extends Component {
       start: this.props.dateRange.start,
       end: this.props.dateRange.end,
     });
+    console.log("this.props from form", this.props);
   }
+  handleHotel = () => {
+    // const { hotelFlag } = this.state;
+    this.setState({
+      hotelFlag: !this.state.hotelFlag,
+    });
+  };
   handleFirstName = (event) => {
     this.setState({
       firstName: event.target.value,
@@ -46,7 +56,7 @@ export class Form extends Component {
     this.setState({
       contact: event.target.value,
     });
-    console.log(event.target.value)
+    console.log(event.target.value);
   };
   handleGender = (event) => {
     this.setState({
@@ -68,11 +78,8 @@ export class Form extends Component {
     return (
       <div className="container-form">
         <div className="form">
-          <div className="form-date">
-          
-            <Button className="ui button">
-              Back
-            </Button>
+          {/* <div className="form-date">
+            <Button className="ui button">Back</Button>
             <div className="final-date">
               <span className="push">
                 <h6> Check-in / Check-out</h6>
@@ -88,59 +95,95 @@ export class Form extends Component {
                 id="datepickeridform"
               ></DateRangePickerComponent>
             </div>
-          </div>
+          </div> */}
           <div>
             <div>
-            {/* <img src = {ImageOne} style={{ width: "100%" }}></img> */}
+              {/* <img src = {ImageOne} style={{ width: "100%" }}></img> */}
             </div>
-           <div className="form-design">
-           <form >
-            <h2>Book your Room</h2>
-           
-            <div className="form-contents">
-              <div className="d-flex form-contents1">
-              {/* <label>First Name</label> */}
-              <input
-                type="text"
-                placeholder="First Name"
-                value={this.state.firstName}
-                onChange={this.handleFirstName}
-              ></input>
-              {/* <label type="text"  placeholder="Last Name">
+            <div className="form-design">
+              <form>
+                <h2>Book your Room</h2>
+                <div className="hotel-Details">
+                  <div className="hotelHeading">
+                    <h1>Hotel Details</h1>
+                    <div>
+                      <button onClick={this.handleHotel}>
+                        <KeyboardArrowDownIcon />
+                      </button>
+                    </div>
+                  </div>
+                  {/* {this.state.hotelFlag && <HotelDetail />} */}
+                  <div className="hotelDetailContainer">
+                    <div className="firstContainer">
+                      <div className="hotelImage">hotelImage</div>
+                      <div>
+                        <div>
+                          <h4>Crown </h4>
+                        </div>
+                        <div>3/5</div>
+                        <div>
+                          <h5>Chennai ,India</h5>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="secondContainer">
+                      <div>
+                        <h3>Check-in Date</h3>
+                      </div>
+                      <div>
+                        <h3>Check-ot Date</h3> 2 june 20221
+                      </div>
+
+                      <div>
+                        <h3>Rooms</h3> 1
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-contents">
+                  <div className="d-flex form-contents1">
+                    {/* <label>First Name</label> */}
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      value={this.state.firstName}
+                      onChange={this.handleFirstName}
+                    ></input>
+                    {/* <label type="text"  placeholder="Last Name">
                 Last Name
               </label> */}
-              <input
-                type="text"
-                placeholder="Last Name"
-                value={this.state.lastName}
-                onChange={this.handleLastName}
-              ></input>
-              <br />
-              {/* <label>Email Address</label> */}
-              <input
-                type="email"
-                placeholder="E-mail"
-                value={this.state.email}
-                onChange={this.handleEmail}
-              ></input>
-              </div>
-              <br />
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      value={this.state.lastName}
+                      onChange={this.handleLastName}
+                    ></input>
+                    <br />
+                    {/* <label>Email Address</label> */}
+                    <input
+                      type="email"
+                      placeholder="E-mail"
+                      value={this.state.email}
+                      onChange={this.handleEmail}
+                    ></input>
+                  </div>
+                  <br />
 
-              <div className="d-flex form-contents1" >
-              {/* <label>Mobile No.</label> */}
-              <input
-                type="number"
-                placeholder="Mobile"
-                value={this.state.contact}
-                onChange={this.handleContact}
-              ></input>
-              <br />
-              <div className="GENDER"></div>
-              <select name="Gender" id="gender-select">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              </select>
-              {/* <label>Gender</label>
+                  <div className="d-flex form-contents1">
+                    {/* <label>Mobile No.</label> */}
+                    <input
+                      type="number"
+                      placeholder="Mobile"
+                      value={this.state.contact}
+                      onChange={this.handleContact}
+                    ></input>
+                    <br />
+                    <div className="GENDER"></div>
+                    <select name="Gender" id="gender-select">
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                    {/* <label>Gender</label>
               <label className="d-flex">
                 Male
                 <input
@@ -159,33 +202,28 @@ export class Form extends Component {
                   onChange={this.handleGender}
                 ></input>
               </label> */}
-              </div>
-      
-              
-              {/* <br></br> */}
-              {/* <div className="form-contents1"> */}
-              {/* <h4>Billing address</h4> */}
-              {/* <label>Address</label> */}
-              <div className="d-flex form-contents2">
-              <input
-              className="form-contents5"
-                type="text-area"
-                placeholder="Address"
-                value={this.state.address}
-                onChange={this.handleAddress}
-              ></input>
-              </div>
-              <div className="submit-form">
-                        
-                          <button>Submit</button>
-                       
-                      </div>
+                  </div>
+
+                  {/* <br></br> */}
+                  {/* <div className="form-contents1"> */}
+                  {/* <h4>Billing address</h4> */}
+                  {/* <label>Address</label> */}
+                  <div className="d-flex form-contents2">
+                    <input
+                      className="form-contents5"
+                      type="text-area"
+                      placeholder="Address"
+                      value={this.state.address}
+                      onChange={this.handleAddress}
+                    ></input>
+                  </div>
+                  <div className="submit-form">
+                    <button>Submit</button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
-           </div>
           </div>
-          
-         
         </div>
       </div>
     );
@@ -194,6 +232,9 @@ export class Form extends Component {
 const mapStateToProps = (state) => ({
   dateRange: get(state, "dateRange", []),
   roomRange: get(state, "roomRange", []),
+  roomDetailsList: get(state, "roomDetailsList", []),
+  roomTypeRatesData: get(state, "roomTypeRatesData", []),
+  propertyList: get(state, "propertyList", []),
 });
 
 export default connect(mapStateToProps, null)(Form);
