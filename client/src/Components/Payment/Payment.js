2f9f069c2c7e1c6401fb4c5d6954d4464cea94f6
Suraj-98-S <suraj.s09@wipro.com>
2021-06-08 21:56:50 +0530

    "payment upi"

--- client/src/Components/Payment/Payment.js
+++ client/src/Components/Payment/Payment.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./payment.css";
-
+import ImageOne from "../BookNow/image3.png"
 export class Payment extends Component {
   constructor(props) {
     super(props);
@@ -12,10 +12,31 @@ export class Payment extends Component {
     return (
       <div>
         <div className="creditCardContainer"></div>
-        <div className="UPIContainer"></div>
+        <div className="UPIContainer">
+          <div className="UPIContents">
+          <h2>Enter UPI ID</h2>
+          <div className="ui input">
+            <input
+            type="text"
+            placeholder="MobileNumber@UPI"
+            ></input>
+            {/* <div className="UPIImage">
+             <img src={ImageOne} style={{ width: "20%" }}></img>
+             </div> */}
+          </div>
+          <div className="UPIImage">
+          <img src={ImageOne} style={{ width: "20%" }}></img>
+          </div>
+          </div>
       </div>
-    );
-  }
+      <div className="PaymentButton">
+        <div>
+          <button className="ui payment button">Payment</button>
+        </div>
+      </div>
+      </div>    
+    )
+  };
 }
 
 export default Payment;
