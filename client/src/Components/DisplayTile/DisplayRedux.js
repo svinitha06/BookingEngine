9533cb45f0502d9b0e23dfad48df430cadef8714
ayushi20184 <ayushi.verma6@wipro.com>
2021-06-08 13:44:35 +0530

    Id added for every room type

--- client/src/Components/DisplayTile/DisplayRedux.js
+++ client/src/Components/DisplayTile/DisplayRedux.js
@@ -283,7 +283,11 @@ class DisplayRedux extends Component {
                     if (data.id === post._id) data.priceO = price;
                   });
                   return (
-                    <div key={index} className="homeContainerOne">
+                    <div
+                      key={index}
+                      className="homeContainerOne"
+                      id={post.roomType}
+                    >
                       <div className="wrapperOne">
                         <div>
                           <img
