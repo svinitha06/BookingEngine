38d2b812e35a2abe42f761f204a772954cdb6165
Suraj-98-S <suraj.s09@wipro.com>
2021-06-10 14:55:06 +0530

    change in index.js file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 // import { property } from "../../src/actions/index";
 // import { roomDetails } from "../../src/actions/index";
-let host="http://localhost:5000"
+// let host="http://localhost:5000"
+var env=process.env.NODE_ENV||'development';
+var host = env === 'development' ? "http://localhost:5000": "temporary url for BED given by devops"
 export const getproperty = async () => {
   const res = await axios.get(`${host}/property/Property`);
   return res.data;
@@ -40,6 +42,8 @@ export const getPostHotelDetails = async (data) => {
   );
   return res;
 };
+
+
 // const getRooomTypes = async () => {
 //   const resp = await axios.get("http://localhost:5000/rooms/getRoomTypes");
 //   return resp.data;
